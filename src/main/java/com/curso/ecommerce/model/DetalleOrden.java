a03281a283dc206cda3d7ab2cfcc575e5e206858
Ronny <ronnychamba96@gmail.com>
2022-02-09 14:10:26 -0500

    Agregar vistas y diseño

--- src/main/java/com/curso/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/curso/ecommerce/model/DetalleOrden.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -29,7 +30,7 @@ public class DetalleOrden {
 	@OneToOne
 	private Orden orden;
 	
-	@OneToOne 
+	@ManyToOne
 	private  Producto producto;
 	
 }
