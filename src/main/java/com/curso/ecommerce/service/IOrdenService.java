cc4d8ed9a38d8073690991816b7ecd64eebd450a
Ronny <ronnychamba96@gmail.com>
2022-02-10 16:56:29 -0500

    metodo para generar numero de orden

--- src/main/java/com/curso/ecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -1,7 +1,14 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
+
 import com.curso.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	
 	Orden save(Orden orden);
+	
+	List<Orden> findAll();
+	
+	String generaNumeroOrden();
 }
--- src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,9 @@
 package com.curso.ecommerce.service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,14 +11,72 @@ import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.repository.IOrdenRepository;
 
 @Service
-public class OrdenServiceImpl   implements IOrdenService{
+public class OrdenServiceImpl implements IOrdenService {
 
 	@Autowired
 	private IOrdenRepository ordenRepository;
 
 	@Override
 	public Orden save(Orden orden) {
-	
+
 		return ordenRepository.save(orden);
 	}
+
+	@Override
+	public List<Orden> findAll() {
+
+		return ordenRepository.findAll();
+	}
+
+	/**
+	 * Metodo encargado de generar el nuevo numero de orden
+	 * @return : El nuevo numero de orden convertido a String
+	 */
+	@Override
+	public String generaNumeroOrden() {
+
+		Integer numero = 0;
+		String numeroConcatenado = "";
+
+		List<Orden> ordenes = findAll();
+
+		List<Integer> numeros = ordenes.stream().map(od -> Integer.parseInt(od.getNumero()))
+				.collect(Collectors.toList());
+
+		numero = ordenes.isEmpty() ? 1 : (numeros.stream().max((num1, num2) -> Integer.compare(num1, num2)).get() + 1);
+
+		numeroConcatenado = getNuevoNumeroOrden(numero);
+		
+		return numeroConcatenado;
+	}
+
+	/**
+	 * 
+	 * @param numOrden : Numero para generar el nuevo numero de orden
+	 * @return : Un String con el patron nuevo para el numero de orden
+	 */
+	private String getNuevoNumeroOrden(Integer numOrden) {
+
+		String newNumOrden = "";
+
+		// Modelo numero; 0000000001
+		if (numOrden < 10) {
+			newNumOrden = "000000000".concat(String.valueOf(numOrden));
+			
+		} else if (numOrden < 100) {
+			newNumOrden = "00000000".concat(String.valueOf(numOrden));
+		
+		} else if (numOrden < 1000) {
+			newNumOrden = "0000000".concat(String.valueOf(numOrden));
+		
+		} else if (numOrden < 10000) {
+			newNumOrden = "000000".concat(String.valueOf(numOrden));
+		
+		} else {
+			newNumOrden = "00000".concat(String.valueOf(numOrden));
+		}
+
+		return newNumOrden;
+	}
+
 }
